cb1a9b94f6465d8bf312505053de1a1a75e60d8d
nicolas <mazzola8@hotmail.com>
2016-01-14 22:22:40 +0100

    modif pages navbar

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -12,9 +12,9 @@
     <div>
         <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
             <div class="text-center">
-               <h2>Rouge Alex - Mazolla Nicolas</br>
+               <h2>Rouge Alex - Mazzola Nicolas</br>
                TR3TIN - Techniques Informatiques - 2015-2016</br>
-               <a href="https://github.com/RedAlixius/Site_ToDo">Lien Git Hub</a>
+               <a href="https://github.com/RedAlixius/Site_ToDo">Lien GitHub</a>
                </h2>
             </div>
         </div>
